8c6b09a590e7597afa2de3172db71c206523110c
202113 <quicklast.com@gmail.com>
2023-10-26 10:07:20 +0300

    Working on adding photos

---
+++ OsipovKirill320_KittenDogApp/Pages/AddPhoto.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using OsipovKirill320_KittenDogApp.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OsipovKirill320_KittenDogApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPhoto.xaml
+    /// </summary>
+    public partial class AddPhoto : Page
+    {
+        public static Photo photo = new Photo();
+        public static User userToSend = new User();
+        public AddPhoto(User user)
+        {
+            InitializeComponent();
+            userToSend = user;
+        }
+
+        private void PhotoAdd_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                Filter = "*.png|*.png|*.jpg|*.jpg|*.jpeg|*.jpeg"
+            };
+            if (openFileDialog.ShowDialog().GetValueOrDefault())
+            {
+                photo.PhotoSource = File.ReadAllBytes(openFileDialog.FileName);
+                PhotoCheck.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage(userToSend));
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTBx.Text.Trim();
+            string description = DescriptionTBx.Text.Trim();
+        }
+    }
+}
--- OsipovKirill320_KittenDogApp/Pages/MainPage.xaml.cs
+++ OsipovKirill320_KittenDogApp/Pages/MainPage.xaml.cs
@@ -21,18 +21,48 @@ namespace OsipovKirill320_KittenDogApp.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        public static int userID = 0;
+        public static List<Photo> Photos { get; set; }
+        public static User userToSend = new User(); 
         public MainPage(User user)
         {
             InitializeComponent();
+            userToSend = user;
             if (user.IDUser == 1)
-                PhotoLV.ItemsSource = DBConn.KDEnt.Photo.Where(i => i.ForUser == 1).ToList();
+            {
+                profileImage.Source = new BitmapImage(new Uri("/Images/pyroProfile.jpg", UriKind.RelativeOrAbsolute));
+                WelcomeTBx.Text = "Привет, Пиро!";
+                userID = 1;
+            }
             if (user.IDUser == 2)
-                PhotoLV.ItemsSource = DBConn.KDEnt.Photo.Where(i => i.ForUser == 2).ToList();
+            {
+                profileImage.Source = new BitmapImage(new Uri("/Images/delyaProfile.jpg", UriKind.RelativeOrAbsolute));
+                WelcomeTBx.Text = "Привет, Деля!";
+                userID = 2;
+            }
+                
+            PhotoLV.ItemsSource = DBConn.KDEnt.Photo.Where(i => i.ForUser == userID).ToList();
+            Photos = DBConn.KDEnt.Photo.Where(i => i.ForUser == userID).ToList();
+            this.DataContext = this;
         }
 
         private void SortTBx_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (SortTBx.Text.Trim() == "")
+                PhotoLV.ItemsSource = DBConn.KDEnt.Photo.Where(i => i.ForUser == userID).ToList();
+            else
+                PhotoLV.ItemsSource = DBConn.KDEnt.Photo.Where(i => i.Description.Contains(SortTBx.Text.Trim()) && i.ForUser == userID).ToList();
+        }
 
+        private void SortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var t = (SortCB.SelectedItem as Photo).Name;
+            PhotoLV.ItemsSource = DBConn.KDEnt.Photo.Where(i => i.Name == t).ToList();
+        }
+
+        private void AddPhoto_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainPage(userToSend));
         }
     }
 }

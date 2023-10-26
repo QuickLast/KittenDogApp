28fab801d41255879d2ae1299b4903fedda1fcec
202113 <quicklast.com@gmail.com>
2023-10-26 08:21:20 +0300

    Added pages and DB Model

---
+++ OsipovKirill320_KittenDogApp/Model/DBConn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsipovKirill320_KittenDogApp.Model
+{
+    public class DBConn
+    {
+        public KittenDogDBEntities KDEnt = new KittenDogDBEntities();
+    }
+}
